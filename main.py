cffcde4bd511be94d25c1730a6b01595a6719847
AlexGudis <gudis2003@mail.ru>
2025-05-24 12:48:21 +0300

    generate xml output

--- main.py
+++ main.py
@@ -54,6 +54,34 @@ def build_hierarchy(classes, aggregations):
     return multiplicity_map
 
 
+def generate_config_xml(classes, root_class_name, multiplicity_map, indent=4):
+    def build_xml(class_name, level=0):
+        class_info = classes[class_name]
+        indent_str = ' ' * (level * indent)
+        xml_lines = []
+        
+        xml_lines.append(f"{indent_str}<{class_name}>")
+        
+        for attr in class_info['attributes']:
+            xml_lines.append(f"{indent_str}{' ' * indent}<{attr['name']}>{attr['type']}</{attr['name']}>")
+        
+        for child in sorted(class_info['children']):
+            xml_lines.extend(build_xml(child, level + 1))
+        
+        xml_lines.append(f"{indent_str}</{class_name}>")
+        
+        return xml_lines
+    
+    root_class = next((c for c in classes.values() if c['isRoot']), None)
+    if not root_class:
+        root_class = classes.get(root_class_name)
+    
+    if not root_class:
+        raise ValueError("No root class found")
+    
+    xml_lines = build_xml(root_class['name'])
+    return '\n'.join(xml_lines)
+
 
 def main():
     input_file = 'impulse_test_input.xml'
@@ -66,11 +94,16 @@ def main():
 
     multiplicity_map = build_hierarchy(classes, aggregations)
 
+    #print(classes)
+    #print(aggregations)
+
+    root_class = next((c['name'] for c in classes.values() if c['isRoot']), None)
+    if not root_class:
+        root_class = 'BTS'
     
-
-    print(classes)
-
-    print(aggregations)
+    config_xml = generate_config_xml(classes, root_class, multiplicity_map)
+    with open('./out/config.xml', 'w') as f:
+        f.write(config_xml)
 
 if __name__ == '__main__':
     main()
